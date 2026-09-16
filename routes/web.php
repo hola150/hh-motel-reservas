23c56521dde879912a6fcc76ec95152b59af3876
hola150 <hola150@users.noreply.github.com>
2026-09-16 00:06:28 -0300

    Sacar ruta de diagnostico temporal

--- routes/web.php
+++ routes/web.php
@@ -30,19 +30,6 @@ use App\Http\Controllers\RoomInspectionController;
 use App\Http\Controllers\CalendarController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/_diag', function () {
-    return response()->json([
-        'has_shifts_route' => Route::has('admin.shifts.index'),
-        'has_staff_route' => Route::has('admin.staff.index'),
-        'shift_controller_exists' => class_exists(\App\Http\Controllers\Admin\ShiftController::class),
-        'staff_model_exists' => class_exists(\App\Models\Staff::class),
-        'staff_table_exists' => \Illuminate\Support\Facades\Schema::hasTable('staff'),
-        'shifts_table_exists' => \Illuminate\Support\Facades\Schema::hasTable('shifts'),
-        'route_count' => count(Route::getRoutes()->getRoutes()),
-        'php_version' => PHP_VERSION,
-    ]);
-});
-
 Route::get('/', function () {
     return redirect()->route('rooms.board');
 });
